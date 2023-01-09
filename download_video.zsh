6873786f5b3f3d792f4a7b2b47e7be43dd894926
melvinrajendran <melvin.rajendran@gmail.com>
2023-01-08 22:55:21 -0500

    Update zsh script

--- download_video.zsh
+++ download_video.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 # Change the current directory to that which contains the Python script
-cd /Users/melvinrajendran/Documents/Video Downloader
+cd "/Users/melvinrajendran/Documents/Video Downloader"
 # Run the script using one command line argument
 python3 index.py $1
